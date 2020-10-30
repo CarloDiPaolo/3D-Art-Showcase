e36096c5e35d283889ed7d27bb72019f4da6105f
Benjamin Rose <rose.benjamin@t-online.de>
2020-10-30 14:50:29 +0100

    Built basic showcase scene and first camera code setup

---
+++ Art Showcase/Assets/Scripts/Project/CameraBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : Singleton<CameraBehaviour>
+{
+    private Camera cam = null;
+    private Animator anim = null;
+
+    private void Start()
+    {
+        cam = this.GetComponent<Camera>();
+        anim = this.GetComponent<Animator>();
+    }
+
+    public bool TryChangeCameraPosition()
+    {
+        Debug.Log("Trying to change Camera Position");
+
+        if(anim.GetCurrentAnimatorClipInfo(0)[0].clip.name == "PositionA")
+        {
+            Debug.Log("a to b");
+            anim.SetTrigger("ChangePos");
+            return true;
+        }
+        else if(anim.GetCurrentAnimatorClipInfo(0)[0].clip.name == "PositionB")
+        {
+            Debug.Log("b to a");
+            anim.SetTrigger("ChangePos");
+            return true;
+        }
+        else
+        {
+            Debug.Log("In Transition");
+            return false;
+        }
+    }
+}
---
+++ Art Showcase/Assets/Scripts/Project/ShowcaseMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowcaseMenu : MonoBehaviour
+{
+    private bool open = true;
+    [SerializeField] GameObject menuParent;
+
+    private void Start()
+    {
+        if(menuParent == null)
+        {
+            Debug.LogError("MenuParent not set in ShowcaseMenu!");
+        }
+        else
+        {
+            menuParent.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(CameraBehaviour.Instance.TryChangeCameraPosition())
+            {
+                menuParent.SetActive(true);
+            }
+        }
+    }
+}
